f134c59e0204fed05750dc8224b01a8fbcfbb499
Paul Chibamba <paulchibamba@gmail.com>
2026-09-12 04:33:11 +0200

    test: deliberate type error to watch CI fail

--- app/src/main/java/com/paulchibamba/ci_playground/MainActivity.kt
+++ app/src/main/java/com/paulchibamba/ci_playground/MainActivity.kt
@@ -24,6 +24,8 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+
+                    val x: Int = "nope"
                 }
             }
         }
